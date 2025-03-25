fa70fd6b689547eb5d1096aa4f7dbc6677ac6304
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2025-03-25 13:06:37 +0000

    new: add mcp cvd & avs

---
+++ src/Vault/Value/Avs.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CraigPaul\Moneris\Vault\Value;
+
+class Avs
+{
+	public function __construct(
+		public readonly string $streetNumber,
+		public readonly string $streetName,
+		public readonly string $postalCode,
+	) {}
+
+	public function toArray(): array
+	{
+		return [
+			'avs_street_number' => $this->streetNumber,
+			'avs_street_name' => $this->streetName,
+			'avs_zipcode' => $this->postalCode,
+		];
+	}
+}
